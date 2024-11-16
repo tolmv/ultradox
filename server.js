e51532ea51502169e89acea988eb4c15aeff4964
Ivan Tolmachev <37975884+tolmv@users.noreply.github.com>
2024-11-16 23:48:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
